c3b2120ffe5b6c07044d93be3353c7c51614158a
hao.li <mythlee23@gmail.com>
2026-06-05 00:43:41 +0800

    feat: 添加网易云插件 NeteasePlugin

---
+++ src/main/java/com/xiahaimoyu/musicconverter/plugin/netease/NeteasePlugin.java
@@ -0,0 +1,37 @@
+package com.xiahaimoyu.musicconverter.plugin.netease;
+
+import com.xiahaimoyu.musicconverter.plugin.AbstractPlugin;
+import com.xiahaimoyu.musicconverter.plugin.Decoder;
+import com.xiahaimoyu.musicconverter.plugin.MetadataHandler;
+
+import java.util.Set;
+
+/**
+ * 网易云音乐插件
+ * <p>
+ * 支持 NCM 加密格式的解码和元数据处理
+ */
+public final class NeteasePlugin extends AbstractPlugin {
+
+    private static final Set<String> EXTENSIONS = Set.of("ncm");
+
+    @Override
+    public String pluginName() {
+        return "Netease Cloud Music";
+    }
+
+    @Override
+    public Set<String> supportedExtensions() {
+        return EXTENSIONS;
+    }
+
+    @Override
+    protected Decoder createDecoderInstance() {
+        return new NcmDecoder();
+    }
+
+    @Override
+    protected MetadataHandler createMetadataHandlerInstance() {
+        return new NcmMetadataHandler();
+    }
+}
